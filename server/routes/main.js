b564fbc391760b125ca69750c711c760b43ecc47
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-01-28 16:29:41 +0500

    no match found

--- server/routes/main.js
+++ server/routes/main.js
@@ -97,7 +97,8 @@ router.post('/search', async (req, res) => {
         });
         res.render("search",{
             data,
-            locals 
+            locals, 
+            searchTerm
         });
 
     }
