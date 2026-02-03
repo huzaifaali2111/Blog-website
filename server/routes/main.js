792859bc6e1bc6cba726f0feece78b6eda9e2c82
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-02-03 18:21:13 +0500

    pata nhi

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router();    
 const Post = require('../models/post');
 const Comment = require('../models/comments');
 const Contact = require('../models/contact');
